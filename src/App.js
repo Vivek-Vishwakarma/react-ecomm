a0479b488686b7eed97002cbc1e275d8a1e1fbc2
Vivek Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2022-06-06 16:49:02 +0530

    added redux basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
  // eslint-disable-next-line
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+  const inc = () =>{
+    dispatch({type : "INC"})
+  }
+  const dec = () =>{
+    dispatch({type : "DEC"})
+  }
+  const add = () => {
+    dispatch({type : "ADD", payload : 10})
+  }
+  
   return (
     <>
-      Hello
+      {counter}
+      <br />
+      <button onClick={inc}>Increment</button>
+      <button onClick={dec}>Decrement</button>
+      <button onClick={add}>Add by 10</button>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/index"
 ReactDOM.render(
-  <React.StrictMode>
-
+  <Provider store={store}>
+    <React.StrictMode>
       <App />
+    </React.StrictMode>
+  </Provider>,
 
-  </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
