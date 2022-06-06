ec23aa52acd53a94134333e96fbfa97c14ff7f7a
Vivek Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2022-06-06 14:19:43 +0530

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Mynavbar from "./components/Mynavbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
-import CartContext from "./context/CartContext";
+ // eslint-disable-next-line
+import React from 'react';
 function App() {
   return (
     <>
-      <CartContext>
-        <Router>
-          <Mynavbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </Router>
-      </CartContext>
+      Hello
     </>
   );
 }
--- src/context/Cart.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const Cart = createContext()
-
-export default Cart
